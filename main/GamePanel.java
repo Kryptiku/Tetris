5f67d2b6886cb1cf97e8ea9d652245cef1fee765
Aldrich Ryan Antony <54350232+Kryptiku@users.noreply.github.com>
2024-09-14 20:12:44 +0800

    Added Rectangular Box

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -10,6 +11,7 @@ public class GamePanel extends JPanel implements Runnable{
     public static final int HEIGHT = 720;
     final int FPS = 60;
     Thread gameThread;
+    PlayManager pm;
 
 
     public GamePanel() {
@@ -18,6 +20,9 @@ public class GamePanel extends JPanel implements Runnable{
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.black);
         this.setLayout(null);
+
+        pm = new PlayManager();
+
 
     }
     public void launchGame() {
@@ -51,9 +56,14 @@ public class GamePanel extends JPanel implements Runnable{
 
     }
     private void update() {
+
+        pm.update();
         
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+        pm.draw(g2);
     }
 }
--- main/PlayManager.java
+++ main/PlayManager.java
@@ -1,3 +1,34 @@
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.BasicStroke;
+
 public class PlayManager {
+
+    //Main Play Area
+    final int WIDTH = 360;
+    final int HEIGHT = 600;
+    public static int left_x;
+    public static int right_x;
+    public static int top_y;
+    public static int bottom_y;
+
+    public PlayManager() {
+        // Main Play Area Frame
+        left_x = (GamePanel.WIDTH/2) - (WIDTH/2); // 1280/2 - 360/2 = 460
+        right_x = left_x + WIDTH;
+        top_y = 50;
+        bottom_y = top_y + HEIGHT;
+    }
+    public void update() {
+
+    }
+    public void draw(Graphics2D g2) {
+
+        // Draw Play Area Frame
+        g2.setColor(Color.white);
+        g2.setStroke(new BasicStroke(4f));
+        g2.drawRect(left_x-4, top_y-4, WIDTH+8, HEIGHT+8);
+
+    }
 
 }
